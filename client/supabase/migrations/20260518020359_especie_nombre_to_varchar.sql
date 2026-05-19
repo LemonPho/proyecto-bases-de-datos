7bf3ca78c0da1fa78c204548c66980ff06a8f128
Louis Day <luigiday@amazon.com>
2026-05-19 13:27:50 -0600

    eliminar animales

--- client/supabase/migrations/20260518020359_especie_nombre_to_varchar.sql
+++
@@ -1,9 +0,0 @@
--- Cambiar la columna nombre_especie de enum a VARCHAR(50) para permitir
--- nombres de especie arbitrarios en lugar de un conjunto cerrado.
-
-ALTER TABLE especies
-    ALTER COLUMN nombre_especie TYPE VARCHAR(50)
-    USING nombre_especie::text;
-
--- El enum ya no se usa en ninguna otra tabla, se puede eliminar.
-DROP TYPE IF EXISTS especie_nombre_enum;
